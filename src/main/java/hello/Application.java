29e1908812c0a2ce10b7ae6f04af47eeb16079f3
jigneshmehta1984 <38507328+jigneshmehta1984@users.noreply.github.com>
2018-10-26 12:32:33 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hello Docker World - 1st Docker Deployment Test Done";
     }
 
     public static void main(String[] args) {
